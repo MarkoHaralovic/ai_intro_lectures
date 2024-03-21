3bb3df73a52537d86429a04d89455255c29a9d58
MarkoHaralovic <mharalovic11@gmail.com>
2024-03-21 07:57:04 +0100

    Finished A* algorithm

--- lw1/0036538058/lab1py/solution.py
+++ lw1/0036538058/lab1py/solution.py
@@ -1,6 +1,7 @@
 import getopt
 import sys
 import os
+from queue import PriorityQueue
 
 algorithms = ['bfs','ucs','astar']
 # --ss: path to state space descriptor file,
@@ -25,15 +26,15 @@ if '--alg' in argumentList:
                print('Error: --h is missing')
                sys.exit()
             else:
-               h = argumentList[argumentList.index('--h') + 1]
+               h_file = argumentList[argumentList.index('--h') + 1]
          ss = argumentList[argumentList.index('--ss') + 1]
 elif '--ss' and '--h'  and 'check-optimistic' in argumentList:
    ss = argumentList[argumentList.index('--ss') + 1]
-   h = argumentList[argumentList.index('--h') + 1]
+   h_file = argumentList[argumentList.index('--h') + 1]
    check_optimistic = True
 elif '--ss' and '--h'  and 'check-consistent' in argumentList:
    ss= argumentList[argumentList.index('--ss') + 1]
-   h = argumentList[argumentList.index('--h') + 1]
+   h_file= argumentList[argumentList.index('--h') + 1]
    check_consistent = True
 
 #Got the code from https://stackoverflow.com/questions/10487563/unicode-error-handling-with-python-3s-readlines
@@ -41,9 +42,12 @@ state_space_file = open(ss, 'r', encoding='utf-8', errors='ignore')
 ss_lines = state_space_file.readlines()
 
 if alg =='astar':
-   heuristics_file = open(h, 'r', encoding='utf-8', errors='ignore')
+   heuristics_file = open(h_file, 'r', encoding='utf-8', errors='ignore')
    h_lines = heuristics_file.readlines()
-   
+   h_lines = [line.strip("\n") for line in h_lines]
+   # print(h_lines)  
+   h = {line.split(": ")[0]:int(line.split(": ")[1]) for line in h_lines}
+
 def init_state_space(ss_lines):
    state_transitions = {}
    initial_state, goal_states= None, None
@@ -66,6 +70,7 @@ def init_state_space(ss_lines):
    return initial_state, goal_states, state_transitions
 
 initial_state, goal_states, state_transitions = init_state_space(ss_lines)
+
 
 def find_path_bfs(state, parent):
     path = []
@@ -138,10 +143,53 @@ def ucs(s0, succ, goal):
 
     return None, False, None, len(visited), None, None
  
+def a_star(s0,succ,goal,h):
+   if s0 in goal:
+      return s0, True, 0, 1, 0, [s0]
+
+   open = [(s0, 0, 0)]
+   closed = {}
+   parent = {s0: None}
+
+   while open:
+      state,g,f = open.pop(0)
+      if state in goal:
+         path = find_path_ucs(state, parent)
+         return state, True, len(path), len(closed.keys()), g, path 
+      if state not in closed.keys():
+         closed[state] = g
+         # print(closed)
+      for m_state,g_cost in succ[state].items():
+         h_cost = h[m_state] if m_state in h else None
+         
+         total_g = g + g_cost
+         # print(m_state,total_g,h_cost)
+         if any(m_state == x[0] and total_g < x[1] for x in open):
+            open = [x for x in open if x[0] != m_state]
+            open.append((m_state, total_g, h_cost))
+            del closed[m_state] 
+            del parent[m_state]
+         if m_state in closed.keys():
+            if closed[m_state] <= total_g:
+               continue
+            else:
+               del closed[m_state] 
+               del parent[m_state]
+               
+         closed[m_state] = total_g
+         parent[m_state] = state
+         open.append((m_state,total_g,total_g+h_cost))
+         open.sort(key=lambda x: x[2])  
+   
+            
+         
+      
+   return 
 if alg == 'bfs':
    x_state,found,x_depth,visited,x_cost,path = bfs(initial_state,state_transitions,goal_states)
+   response = 'yes' if found else 'no'
    output = f"# BFS {ss.split('/')[-1]}\n"
-   output+= f"[FOUND_SOLUTION]: {found}\n"
+   output+= f"[FOUND_SOLUTION]: {response}\n"
    output+= f"[STATES_VISITED]: {visited}\n"
    output+= f"[PATH_LENGTH]: {x_depth}\n"
    output+= f"[TOTAL_COST]: {x_cost}\n"
@@ -149,19 +197,24 @@ if alg == 'bfs':
    print(output)
 elif alg == 'ucs':
    x_state,found,x_depth,visited,x_cost,path = ucs(initial_state,state_transitions,goal_states)
+   response = 'yes' if found else 'no'
    output = f"# UCS {ss.split('/')[-1]}\n"
-   output+= f"[FOUND_SOLUTION]: {found}\n"
+   output+= f"[FOUND_SOLUTION]: {response}\n"
    output+= f"[STATES_VISITED]: {visited}\n"
    output+= f"[PATH_LENGTH]: {x_depth}\n"
    output+= f"[TOTAL_COST]: {x_cost}\n"
    output+= f"[PATH]: {' => '.join(path)}"
    print(output)
+elif alg =='astar':
+   x_state,found,x_depth,visited,x_cost,path = a_star(initial_state,state_transitions,goal_states,h)
+   output = f"# x_state : {x_state}\n"
+   response = 'yes' if found else 'no'
+   output = f"# A-STAR {h_file.split('/')[-1]}\n"
+   output+= f"[FOUND_SOLUTION]: {response}\n"
+   output+= f"[STATES_VISITED]: {visited}\n"
+   output+= f"[PATH_LENGTH]: {x_depth}\n"
+   output+= f"[TOTAL_COST]: {x_cost}\n"
+   output+= f"[PATH]: {' => '.join(path)}"
+   print(output)
+   pass
    
-#BFS
-#AI fail_course True  6 3 21  enroll_artificial_intelligence => fail_lab => fail_course
-#ISTRA Buzet True 11(17) 5 100 Pula => Barban => Labin => Lupoglav => Buzet
-                  #11 if we implement depth dict, in that case AI return 4 3 21  
-
-#ucs
-#AI pass_course True  7 4 17  enroll_artificial_intelligence => complete_lab => pass_continuous =>pass_course
-#Istra Buzet True 17 5 100 Pula => Barban => Labin => Lupoglav => Buzetgit 
