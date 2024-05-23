95830b6189d2bdf36d9420ce20cb98b1976a8454
MarkoHaralovic <mharalovic11@gmail.com>
2024-05-23 11:44:38 +0200

    Needs print fixing, handling feature not in the dataset and depth handling

---
+++ lw3/0036538058/lab3py/DecisionTreeID3.py
@@ -0,0 +1,68 @@
+from DecisionTreeInterface import DecisionTree
+from Dataset import TrainDataset, TestDataset, Leaf,Node
+from metrics import informationGain,entropy
+import numpy as np
+
+class DecisionTreeID3(DecisionTree):
+   def __init__(self,max_depth:int=None):
+      self.max_depth = max_depth
+      self.tree = {}
+      
+   def fit(self,train_dataset : TrainDataset):
+      self.tree = self.id3(D = train_dataset,D_parent=train_dataset,X=train_dataset.get_feature_names(),y=train_dataset.get_labels(),depth=0)
+   
+   def id3(self,D:TrainDataset,D_parent:TrainDataset,X:list,y:list,depth:int):
+      if D is None or len(D.get_data()) == 0:
+         v = D_parent.most_common_label()
+         return Leaf(v)
+      
+      if self.max_depth is not None and depth >= self.max_depth:
+         v = D.most_common_label()
+         return Leaf(v)
+     
+      v = D.most_common_label()
+      data = D.get_data()
+      mask = (D.get_labels()==v)
+      if X is None  or len(data)==len(data[mask]):
+         return Leaf(v)
+      
+      IGS = [informationGain(D,feature,D.get_features()[feature].unique())[0] for feature in X]
+      if len(IGS)==0:
+         return Leaf(v)
+      x = X[np.argmax(IGS)]
+      # for feature,IG in zip(X,IGS):
+      #    print(f"IGS({feature})={IG}")
+      # print("---------")
+      
+      subtrees = []
+      V = np.unique(data[x])
+      for v in V:
+         new_X = [feature for feature in X if feature != x]
+         t = self.id3(D = D.filter(x,v),D_parent=D,X = new_X,y=y,depth=depth+1)
+         subtrees.append((v,t))
+      return Node(depth+1,x,subtrees)
+   
+   def predict(self,test_dataset:TestDataset):
+      data = test_dataset.get_features()
+      labels = []
+      for i in range(len(data)):
+         node = self.tree
+         while not isinstance(node,Leaf):
+            feature = node.feature
+            value = data[feature].iloc[i]
+            for (v,t) in node.subtrees:
+               if v == value:
+                  node = t
+                  break
+            if feature is None:
+               labels.append(node.v)
+         labels.append(node.v)
+      return labels
+   def print_tree(self):
+    print("[BRANCHES]:")
+    if isinstance(self.tree, Leaf):
+        print(self.tree)
+    else:
+        print(self.tree) 
+
+#output: level:feature_name=feature_value
---
+++ lw3/0036538058/lab3py/metrics.py
@@ -0,0 +1,53 @@
+import math
+from Dataset import Dataset,TrainDataset,TestDataset
+
+def accuracy(predictions, labels):
+    TP = sum(p==l for p,l in zip(predictions,labels))
+    return "{:.5f}".format(round(TP/len(labels),5))
+
+def confusionMatrix(predictions, labels):
+    unique_labels = list(set(labels))
+    matrix = [[0 for i in range(len(unique_labels))] for j in range(len(unique_labels))]
+    for i in range(len(unique_labels)):
+        for j in range(len(unique_labels)):
+            matrix[i][j] = sum([1 for pred, label in zip(predictions, labels) if label == unique_labels[i] and pred == unique_labels[j]])
+    return matrix
+
+def entropy(dataset: Dataset, feature: str, feature_value=None):
+    data = dataset.get_data()
+    labels = dataset.get_labels()
+
+    if feature_value is not None:
+        mask = data[feature] == feature_value
+        data = data[mask]
+        labels = labels[mask]
+
+    if len(labels) == 0:
+        return 0  
+
+    unique_labels = labels.unique()
+
+    _entropy = 0
+    for label in unique_labels:
+        p = sum(labels == label) / len(labels)
+        _entropy += -p * math.log2(p)
+    
+    return _entropy,len(labels)
+
+def informationGain(dataset:Dataset,parentNode:str,childNodes:list):
+    parentNodeEntropy,_ = entropy(dataset,parentNode)
+    childEntropies = [entropy(dataset,parentNode,child) for child in childNodes]
+    ig = parentNodeEntropy - sum([childEntropy[0] * childEntropy[1] / len(dataset) for childEntropy in childEntropies])
+    lowest_entropy_feature = min([childEntropy[0] for childEntropy in enumerate(childEntropies)])
+    return round(ig,4),childNodes[lowest_entropy_feature] 
+
+train_set_path = r"C:\FER\6TH SEMESTER\INTRO_TO_AI\autograder\data\lab3\files\volleyball.csv"
+train_dataset = TrainDataset(train_set_path)
+
+# print(entropy(train_dataset,'temperature','comfortable'))
+# print(entropy(train_dataset,'temperature','hot'))
+# print(entropy(train_dataset,'temperature','cold'))
+
+# print(informationGain(train_dataset,'temperature',['comfortable','hot','cold']))
+
+# print(train_dataset.most_common_label())
